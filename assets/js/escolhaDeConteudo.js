e0e819ba25c79a62d11b2f96294dd66c0ecf8c4c
Vitor Freitas <63727320+VitorExl@users.noreply.github.com>
2024-06-20 22:54:13 -0300

    Função Create Card
    Iniciada a construção da função de criar os cards a partir do banco de dados e tentativa de fazer um carousel

--- assets/js/escolhaDeConteudo.js
+++ assets/js/escolhaDeConteudo.js
@@ -4,10 +4,45 @@
 
 
 let buttonFilterEl = document.querySelectorAll(".filtros");
-console.log(buttonFilterEl);
 
 buttonFilterEl.forEach(element => {
     element.addEventListener("click", () => {
         element.classList.toggle('filtroAtivo');
     })
 });
+
+
+// A função será adicionada a um FOREACH posteriormente
+function criarCardsConteudoFunc(/* Parâmetros a serem adicionados */){
+    let divCardMain = document.createElement("div");
+    divCardMain.setAttribute("class","cardConteudo");
+    //Adicionar append da div
+
+
+    let divAppend = document.createElement("div");
+    divAppend.setAttribute("class","styleCardBorderImg");
+    divAppend.appendChild(divCardMain);
+    //ESTA PARTE SERÁ ADAPTADA PARA A IMAGEM ALTERAR DE ACORDO COM O CURSO
+    /*let imgCurso = document.createElement("img");
+    imgCurso.setAttribute("class", "iconeCursoImagem");
+    imgCurso.setAttribute("src", )*/
+
+
+    let paragrafoNomeCurso = document.createElement("p");
+    paragrafoNomeCurso.innerHTML = /*Parâmetro a ser passado*/ a;
+    paragrafoNomeCurso.setAttribute("class","tituloCurso whiteText");
+    paragrafoNomeCurso.appendChild(divCardMain);
+
+
+    let sectionCardCurso = document.createElement("section");
+    sectionCardCurso.setAttribute("class", "sectionCards");
+    sectionCardCurso.appendChild(divCardMain);
+
+
+    let imgIconUser = document.createElement("img");
+    //imgIconUser.setAttribute("src", /*Adicionar a URL de Acordo com o curso*/ )
+    imgIconUser.setAttribute("class", "iconeCardCurso");
+    imgIconUser.appendChild(sectionCardCurso);
+
+    
+}
